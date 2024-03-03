48721ef35c6be297deec4bb0132aec168bd38a42
Veet Coder <veetcoder@gmail.com>
2024-03-03 11:16:05 +0100

    Update neo-tree configuration to show dotfiles and ignore gitignored files

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -17,7 +17,21 @@ return {
       colorscheme = "gruvbox",
     },
   },
-
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+          },
+        },
+      },
+    },
+  },
   {
     "christoomey/vim-tmux-navigator",
     cmd = {
